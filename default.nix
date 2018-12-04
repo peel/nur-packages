c49949d793e7d8a3382450363e15116822ad09da
Piotr Limanowski <piotr@codearsonist.com>
2018-12-04 08:36:10 +0100

    fixes nur

--- default.nix
+++ default.nix
@@ -1,8 +1,11 @@
 { pkgs ? import <nixpkgs> {} }:
 
-{
+let
+  ppkgs = import ./pkgs { inherit pkgs; };
+  nkgs = pkgs // ppkgs;
+in {
   lib = import ./lib { inherit pkgs; };
   modules = import ./modules;
-  darwin-modules = import ./darwin-modules;
+  darwin-modules = import ./darwin-modules { pkgs = npkgs; };
   overlays = import ./overlays;
-} // import ./pkgs { inherit pkgs; }
+} // ppkgs
