470439db5fa0753f7e1523585e9ded72ea07a3b8
Piotr Limanowski <piotr@codearsonist.com>
2020-07-06 22:08:51 +0200

    purge

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,17 +1,10 @@
 { pkgs }:
 
 {
-  hoverfly = pkgs.callPackage ./development/tools/hoverfly {};
-  ix = pkgs.callPackage ./misc/ix {};
   metals = pkgs.callPackage ./development/tools/metals {};
-  qarma = pkgs.callPackage ./misc/qarma {
-    inherit (pkgs) stdenv fetchFromGitHub pkgconfig;
-    inherit (pkgs.qt5) qtbase qmake qttools qtmacextras qtx11extras;
-  };
   scripts = pkgs.callPackage ./misc/scripts {
     inherit pkgs; inherit (pkgs) stdenv;
   };
-  zenity = pkgs.callPackage ./misc/zenity {};
 } // (if pkgs.stdenv.isDarwin then {
   yabai = pkgs.callPackage ./os-specific/darwin/yabai {
     inherit (pkgs) stdenv fetchFromGitHub;
