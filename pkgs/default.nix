a4032e330ba72b9581676f7a589d07060a4a8c04
Piotr Limanowski <piotr@codearsonist.com>
2020-08-04 15:45:07 +0200

    remove scala stuff

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,15 +1,10 @@
 { pkgs }:
 
 {
-  metals = pkgs.callPackage ./development/tools/metals {};
   scripts = pkgs.callPackage ./misc/scripts {
     inherit pkgs; inherit (pkgs) stdenv;
   };
 } // (if pkgs.stdenv.isDarwin then {
-  yabai = pkgs.callPackage ./os-specific/darwin/yabai {
-    inherit (pkgs) stdenv fetchFromGitHub;
-    inherit (pkgs.darwin.apple_sdk.frameworks) Carbon Cocoa ScriptingBridge;
-  };
   } else {
   rofi-emoji = pkgs.callPackage ./misc/rofi-emoji {};
   rofi-wifi-menu = pkgs.callPackage ./misc/rofi-wifi-menu {};
